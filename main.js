ad73676af03577db53414113a4421744661fde37
soyeniyi801 <naturalbeauty801@gmail.com>
2024-04-16 16:49:12 -0400

    js functionality

---
+++ main.js
@@ -0,0 +1,18 @@
+const btn = document.getElementById("btnAdvice");
+const advice = document.querySelector(".advice-body");
+const adviceHeader = document.querySelector('.advice-header')
+
+btn.addEventListener("click", () => {
+  const getAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      adviceHeader.innerHTML = `Advice #${data.slip.id}`
+      advice.innerHTML = data.slip.advice;
+      console.log(data);
+    } catch (error) {
+      console.log("failed call");
+    }
+  };
+  getAdvice();
+});
